6a325d3087e231541076365b18d2cbcebc11f1d7
Adrian Espinoza <aespinoza@devsu.com>
2022-12-26 23:24:47 -0300

    Login layout

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,3 +1,14 @@
 import { FC } from "react";
+import "./Login.scss";
 
-export const Login: FC = () => <>LOGIN</>;
+export const Login: FC = () => {
+  return (
+    <div className="login-wrapper">
+      <div className="login-form-container">
+        <div className="login-card">
+          <h2 className="login-title">Iniciar Sesión</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
