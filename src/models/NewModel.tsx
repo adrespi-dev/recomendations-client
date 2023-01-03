832ab23d1c7d28dd1ee6aeeded81f5dcb4642bc7
Adrian Espinoza <aespinoza@devsu.com>
2023-01-03 00:52:48 -0300

    Finalizar nuevo modelo

--- src/models/NewModel.tsx
+++ src/models/NewModel.tsx
@@ -1,6 +1,7 @@
 import { Alert, Button, Form, Input } from "antd";
 import { useForm } from "antd/es/form/Form";
-import { FC, useEffect } from "react";
+import Paragraph from "antd/es/typography/Paragraph";
+import { FC, useEffect, useState } from "react";
 import { useMutation, useQueryClient } from "react-query";
 import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
@@ -48,12 +49,17 @@ export const NewModel: FC = () => {
         <div className="new-modal">
           <Form form={form} layout="vertical" onFinish={save}>
             <Form.Item
+              className="with-limit"
               label="Nombre del modelo"
               name="name"
               rules={[
                 { required: true, message: "Ingrese un nombre para el modelo" },
               ]}>
               <Input />
+            </Form.Item>
+
+            <Form.Item style={{ paddingTop: 24 }} label="Tipo de modelo">
+              <ModelTypes />
             </Form.Item>
 
             <Form.Item shouldUpdate>
@@ -83,5 +89,58 @@ export const NewModel: FC = () => {
         </div>
       </div>
     </>
+  );
+};
+
+const ModelTypes: FC = () => {
+  const [modelType, setModelType] = useState("retrieval_and_ranking");
+
+  return (
+    <div className="model-types">
+      <div
+        className={`model-type ${
+          modelType === "retrieval_and_ranking" ? "selected" : ""
+        }`}
+        onClick={() => setModelType("retrieval_and_ranking")}
+        style={{ marginRight: 12 }}>
+        <div className="model-type-title">Retrieval + Ranking</div>
+        <div className="model-type-desc">
+          <Paragraph
+            ellipsis={{
+              rows: 3,
+              expandable: true,
+              symbol: <div>Ver más</div>,
+            }}>
+            Modelo de 2 fases: La fase retrieval y la fase ranking.
+            <br />
+            <br />
+            La <strong>fase retrieval</strong> permite encontrar recomendaciones
+            no tan personalizadas pero eficientes sobre base de datos enormes
+            <br />
+            <br />
+            La <strong>fase ranking</strong> valora las recomendaciones de la
+            fase anterior y genera recomendaciones muy personalizadas y con
+            mucho mas contexto (features)
+          </Paragraph>
+        </div>
+      </div>
+
+      <div
+        className={`model-type ${modelType === "retrieval" ? "selected" : ""}`}
+        onClick={() => setModelType("retrieval")}>
+        <div className="model-type-title">Retrieval</div>
+        <div className="model-type-desc">
+          <Paragraph
+            ellipsis={{
+              rows: 3,
+              expandable: true,
+              symbol: <div>Ver más</div>,
+            }}>
+            Encuentra recomendaciones de manera eficiente sobre bases de datos
+            enormes. No soporta caracteristicas (features)
+          </Paragraph>
+        </div>
+      </div>
+    </div>
   );
 };
